db41f2095ae812dff03773828130565bfb36bc12
Ruth-Ann Blair <blair.ruthie1@gmail.com>
2023-04-10 00:02:58 -0500

    Exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,13 @@
-# Add any form classes for Flask-WTF here
+ # Add any form classes for Flask-WTF here
+ 
+from flask_wtf import FlaskForm, TextAreaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms.validators import InputRequired
+from wtforms import StringField
+
+class MovieForm(FlaskForm):
+    title = StringField('title', validators=[InputRequired()])
+    description = TextAreaField('description', validators=[InputRequired()])
+    poster = FileField('poster',validators=[FileRequired(),FileAllowed(['jpg',
+'png', 'jpeg'], 'Illegal file detected. Ensure your file has a name
+and is in one of the following formats: png, jpg, jpeg.')])
